05754c0cb5957455efed7789542614c2e06f8ed6
Aamir Khan <akhan@paradromics.com>
2020-06-26 17:35:39 -0500

    do not change anything if desired user already exists at UID=1000

--- provision_ubuntu_instance.sh
+++ provision_ubuntu_instance.sh
@@ -7,34 +7,38 @@ pushd /root || exit
 (
 echo "[$(date --iso=s)] First launch detected"
 
-# Check if a user already exists at ID=1000
+# Check the user at UID=1000
 OLD="$(id -n -u 1000 2> /dev/null)"
-if [[ -n "$OLD" ]] ; then
-    echo "Replacing user \"$OLD\" with \"$USR\""
-    usermod "$OLD" --login "$USR" --move-home --home /home/"$USR"
-    groupmod "$OLD" --new-name "$USR"
+if [[ "$OLD" == "$USR" ]]; then
+    echo "[$(date --iso=s)] User \"$OLD\" already exists at UID=100. I won't touch a thing..."
+    :
 else
-    echo "[$(date --iso=s)] Adding new user $USR..."
-    adduser "$USR" --disabled-password --gecos "" --uid 1000
+    if [[ -n "$OLD" ]] ; then
+        echo "[$(date --iso=s)] Replacing user \"$OLD\" with \"$USR\" at UID=1000..."
+        usermod "$OLD" --login "$USR" --move-home --home /home/"$USR"
+        groupmod "$OLD" --new-name "$USR"
+    else
+        echo "[$(date --iso=s)] Adding new user $USR at UID=1000..."
+        adduser "$USR" --disabled-password --gecos "" --uid 1000
+    fi
+    adduser "$USR" sudo
+    # Following is how to generate the salted password hash
+    # openssl passwd -6 | sed -e 's/\$/\\\$/g'
+    # Paste the salted & hashed password inside the double quotes after escaping dollar sign
+    usermod "$USR" -p "\$6\$mad2s9aBKOTmVOeN\$mhoRdRxcr5cFH7V5.BStkXkoOi5oEBSMahPoQxxm4.1z8TARnqlZbTJ9BwNTyY8OCSXoW8hRAw5qZznQVBapE0"
+    # Add SSH keys securely
+    authfile=/home/"$USR"/.ssh/authorized_keys
+    if [[ ! -f "$authfile" ]]; then
+        directory="$(dirname "$authfile")"
+        su aamir -c "mkdir -p \"$directory\""
+        chmod 0700 "$directory"
+        install -m 0600 -o "$USR" -g "$USR" /dev/null "$authfile"
+    fi
+    echo "
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGEHqJmGRD2IwHdNbzAcqv0dpefc/0aJIpTa8jSQMJCM aamir_thinkpad_1569257096
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDHVlHSmjT6YmYlJ/A19vqzDgM1/TS4+6ulEoksgAIXN aamir@macbook_1553227072
+    " | awk '{$1=$1; print}' | grep . >> "$authfile"
 fi
-adduser "$USR" sudo
-# Following is how to generate the password hash
-# openssl passwd -6 | sed -e 's/\$/\\\$/g'
-# Paste the salted & hashed password inside the double quotes after escaping dollar sign
-usermod "$USR" -p "\$6\$mad2s9aBKOTmVOeN\$mhoRdRxcr5cFH7V5.BStkXkoOi5oEBSMahPoQxxm4.1z8TARnqlZbTJ9BwNTyY8OCSXoW8hRAw5qZznQVBapE0"
-
-# Add SSH keys
-authfile=/home/"$USR"/.ssh/authorized_keys
-if [[ ! -f "$authfile" ]]; then
-    directory="$(dirname "$authfile")"
-    su aamir -c "mkdir -p \"$directory\""
-    chmod 0700 "$directory"
-    install -m 0600 -o "$USR" -g "$USR" /dev/null "$authfile"
-fi
-echo "
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGEHqJmGRD2IwHdNbzAcqv0dpefc/0aJIpTa8jSQMJCM aamir_thinkpad_1569257096
-ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDHVlHSmjT6YmYlJ/A19vqzDgM1/TS4+6ulEoksgAIXN aamir@macbook_1553227072
-" | awk '{$1=$1; print}' | grep . >> "$authfile"
 
 echo "[$(date --iso=s)] Updating OS..."
 apt-get update -yq
