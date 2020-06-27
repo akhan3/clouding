b43563bf40d272c04c89f6bf7dc3026e2b7d603d
Aamir Khan <akhan@paradromics.com>
2020-06-27 10:59:59 -0500

    make compatible with sh

--- install_docker.sh
+++ install_docker.sh
@@ -3,21 +3,24 @@
 # https://docs.docker.com/engine/install/ubuntu/
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04
 
+(
 # https://stackoverflow.com/a/18216122/107349
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as sudo"
-  exit 1
+if [ "$(id -u)" -ne "0" ]; then
+    echo "Please run as sudo"
+    exit 1
 fi
 
 apt update -yq
-apt install -yq apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# https://askubuntu.com/a/1013396/11597
+DEBIAN_FRONTEND=noninteractive apt install -yq apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt update -yq
 apt install -yq docker-ce
     # aufs-tools cgroupfs-mount containerd.io docker-ce docker-ce-cli libltdl7 pigz
 systemctl status docker
 adduser aamir docker
+)
 
 # Test
 # docker run hello-world
