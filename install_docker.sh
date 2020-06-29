49b499689baace87bf9e0c82b4153706041917f2
Aamir Khan <akhan@paradromics.com>
2020-06-29 09:07:49 -0500

    split install packages command

--- install_docker.sh
+++ install_docker.sh
@@ -12,7 +12,8 @@ fi
 
 apt update -yq
 # https://askubuntu.com/a/1013396/11597
-DEBIAN_FRONTEND=noninteractive apt install -yq apt-transport-https ca-certificates curl software-properties-common
+apt install -yq apt-transport-https ca-certificates curl
+DEBIAN_FRONTEND=noninteractive apt install -yq software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt update -yq
